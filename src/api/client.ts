910c66ebad78a23a90206bc349011ae61984056f
Dawn Chan <31147639+dawnchan030920@users.noreply.github.com>
2024-12-12 08:23:59 +0000

    [upd] 新增Axios请求和响应拦截器，处理JWT授权和401错误

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,4 +2,29 @@ import axios from "axios";
 
 const client = axios.create({});
 
+client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized!");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default client;
