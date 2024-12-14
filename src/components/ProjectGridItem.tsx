7ea97f246f8b97098caa21d7ebd0340e12ecdeae
Dawn Chan <31147639+dawnchan030920@users.noreply.github.com>
2024-12-14 07:40:03 +0000

    [upd] remove manual window reloading

--- src/components/ProjectGridItem.tsx
+++ src/components/ProjectGridItem.tsx
@@ -27,7 +27,6 @@ export default function ProjectGridItem({
     mutationFn: () => disband(id),
     onSuccess: () => {
       queryClient.invalidateQueries(["projects"]);
-      window.location.reload();
     },
     onError: (error) => {
       console.error(error);
